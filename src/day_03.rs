960c4443648ff86a3d383dcb10e19ebc24b6b375
Danny Revenant <dannyrevenant@gmail.com>
2022-12-05 08:27:56 +0000

    day 03

---
+++ src/day_03.rs
@@ -0,0 +1,56 @@
+use std::collections::HashMap;
+
+const INPUT: &str = include_str!("../input/day_03.txt");
+
+fn get_priority(item: &char) -> usize {
+    let map = {
+        let mut lower: HashMap<char, usize> = ('a'..='z')
+            .into_iter()
+            .map(|c| (c, c as usize - 96))
+            .collect();
+
+        let upper: HashMap<char, usize> = ('A'..='Z')
+            .into_iter()
+            .map(|c| (c, c as usize - 38))
+            .collect();
+        lower.extend(upper);
+        lower
+    };
+
+    *map.get(item).unwrap()
+}
+
+pub fn answer() {
+    let lines: Vec<&str> = INPUT.lines().collect();
+
+    println!(
+        "Part 1: {:?}",
+        lines
+            .iter()
+            .map(|r| {
+                let (comp1, comp2) = r.split_at(r.len() / 2);
+                for c in comp1.chars() {
+                    if comp2.contains(c) {
+                        return get_priority(&c);
+                    }
+                }
+                panic!("Couldn't find a pair.");
+            })
+            .sum::<usize>()
+    );
+
+    println!(
+        "Part 2: {:?}",
+        lines
+            .chunks(3)
+            .map(|chunk| {
+                for c in chunk[0].chars() {
+                    if chunk[1].contains(c) && chunk[2].contains(c) {
+                        return get_priority(&c);
+                    }
+                }
+                panic!("Couldn't find comon item.");
+            })
+            .sum::<usize>()
+    )
+}
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 mod day_01;
 mod day_02;
+mod day_03;
 
 fn main() {
     day_01::answer();
     day_02::answer();
+    day_03::answer();
 }
